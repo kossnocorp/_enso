1b902c8197ce13b14ae50184c7266c52f90374bd
Sasha Koss <koss@nocorp.me>
2015-11-13 20:09:42 +0800

    Add build scripts

--- config/webpack.js
+++ config/webpack.js
@@ -1,7 +1,18 @@
+const path = require('path')
+
+const isTest = process.env.NODE_ENV == 'test'
+
 module.exports = {
-  devtool: 'inline-source-map',
-  entry: {},
-  output: {path: '/'},
+  devtool: isTest ? 'inline-source-map' : 'source-map',
+  entry: isTest ?  {} : {
+    enso: './index'
+  },
+  output: {
+    path: path.join(process.cwd(), 'dist'),
+    filename: '[name].js',
+    library: 'enso',
+    libraryTarget: 'umd'
+  },
   module: {
     loaders: [
       {test: /test\.js$/, loader: 'webpack-espower', exclude: /node_modules/},
